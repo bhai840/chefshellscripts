7dd821ed107272271312a58ca5602dd9fda91c38
bhai840 <bhai840@rediffmail.com>
2018-03-10 13:15:31 -0600

    updated key path

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP="35.172.231.241 54.159.16.189"
-KEY=~/.ec2/splunk-test.pem
+KEY=/var/lib/jenkins/splunk-test.pem
 USER=ubuntu
 for S in $IP ; do
         ssh -i $KEY $USER@$S "ls -l"
